4c755963c468438cefe9451b56467a009897e4bf
Bartlomiej Drapinski <g00428194@atu.ie>
2025-02-02 19:16:53 +0000

    Created TotalCost branch, working on summing up all items and trying out formatting styles for the € sign

--- src/main/java/JustApp.java
+++ src/main/java/JustApp.java
@@ -3,7 +3,7 @@ import ie.atu.week1sem2.Food;
 import ie.atu.week1sem2.Fries;
 import ie.atu.week1sem2.Pizza;
 
-import java.sql.SQLOutput;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 public class JustApp {
@@ -17,23 +17,29 @@ public class JustApp {
         //Add some items burgers, pizzas, fries, salad...
         Burger cheeseBurger = new Burger("Cheese Burger", 1.99, "Beef patty, Cheese, Mayo");
         Pizza pepperoni = new Pizza("Pizza", 9.99, "Pepperoni, Cheese, Ketchup");
-        Fries curry = new Fries("Curly Fries", 4.99, "Ready made - Served Hot");
-        Dessert icecream = new Dessert("Strawberry Ice-cream", 1.99, "Medium - 2 scoops");
+        Fries curly = new Fries("Curly Fries", 4.99, "Ready made - Served Hot");
+        Dessert icecream = new Dessert("Strawberry Ice cream", 1.99, "Medium - 2 scoops");
 
         //Add all the orders to a collection
         items.add(cheeseBurger);
         items.add(pepperoni);
-        items.add(curry);
+        items.add(curly);
         items.add(icecream);
 
         //Display all the orders
+        double totalCost = 0;
         for(Food item : items)
         {
             System.out.println("Name:        " + item.getName());
-            System.out.println("Price:       " + item.getPrice());
+            System.out.println("Price:       €" + item.getPrice());
             System.out.println("Description: " + item.getDescription());
             System.out.println();
+            totalCost += item.getPrice();
         }
+
         // Find the total cost of the whole order
+        //NumberFormat currency = NumberFormat.getCurrencyInstance(java.util.Locale.GERMANY);
+        //System.out.println("Total Cost:  " + currency.format(totalCost));
+        System.out.println("Total cost:  €" + String.format("%.2f", totalCost));
     }
 }
--- src/main/java/ie/atu/week1sem2/Food.java
+++ src/main/java/ie/atu/week1sem2/Food.java
@@ -1,7 +1,26 @@
 package ie.atu.week1sem2;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public interface Food {
     String getName();
     double getPrice();
     String getDescription();
+
+    /*
+    // Experimental code for totalCost formatting
+    default String getPriceFormatted() {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(java.util.Locale.GERMANY);
+        symbols.setDecimalSeparator('€');
+        DecimalFormat formatter = new DecimalFormat("€ #,##0.00", symbols);
+        return formatter.format(getPrice());
+    }
+    default String getPriceFormatted() {
+        NumberFormat currency = NumberFormat.getCurrencyInstance(java.util.Locale.GERMANY);
+        return currency.format(getPrice());
+    }
+    */
 }
